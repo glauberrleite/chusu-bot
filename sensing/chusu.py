61f3271ca42a0366f2ba12b5caa8fd58c95bb94d
JuliaOli <maju.olivi@gmail.com>
2020-07-21 17:55:33 -0300

    chusu simulation pt1

---
+++ sensing/chusu.py
@@ -0,0 +1,41 @@
+import json
+
+class Chusu:
+
+    def __init__(self, number_of_trays):
+        self.number_of_trays = number_of_trays
+        self.list_trays = trays
+
+    def create_Trays(self, numeber_of_trays):
+        return
+    def __str__(self):
+        return self.numeber_of_trays
+
+    def central_server_update(self):
+        # a Python object (dict):
+        x = {
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+        }
+
+        # convert into JSON:
+        y = json.dumps(x)
+
+        # the result is a JSON string:
+        print(y) 
+
+class Tray:
+    
+    def __init__(self, id, color, radius, time, havest):
+        self.id = id
+        self.color = color
+        self.radius = radius
+        self.time = time
+        self.havest = 0
+
+    def Havest(self, color, radius, time):
+        #color menor peso
+        #
+        return 
+
