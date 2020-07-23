ae4329488a78b7c90d066e255efc069326eb2d44
JuliaOli <maju.olivi@gmail.com>
2020-07-22 21:58:50 -0300

    havest function finished

--- sensing/chusu.py
+++ sensing/chusu.py
@@ -1,41 +1,63 @@
 import json
+import numpy as np
+from datetime import date
+
 
 class Chusu:
 
-    def __init__(self, number_of_trays):
-        self.number_of_trays = number_of_trays
-        self.list_trays = trays
+    def __init__(self, chusu_size):
+        self.chusu = np.zeros((3,3))
+        self.trays_list = []
 
-    def create_Trays(self, numeber_of_trays):
+    def new_Tray(self):
+        # searching for an empty space for a new tray
+        empty_tray = np.where(self.chusu_trays == 0)
+        tray_index = list(zip(tray_index[0],tray_index[1]))
+        
+        # initiating a new Tray 
+        # after creating needs to update
+        tray = Tray(self.chusu.size + 1, tray_index[0], 0,0)
+        self.trays_list.append(tray)
+
+        #updating cell status
+        self.chusu_trays[tray_index[0][0]][tray_index[0][1]] = 1
+
+    def update_status(self):
+        for tray in self.trays_list:
+            tray.update_sensors()
+
+    def remove_tray(self, tray_id):
+        #remove tray by id
+        #status in the matrix will be updated to 0
         return
-    def __str__(self):
-        return self.numeber_of_trays
 
-    def central_server_update(self):
-        # a Python object (dict):
-        x = {
-        "name": "John",
-        "age": 30,
-        "city": "New York"
-        }
-
-        # convert into JSON:
-        y = json.dumps(x)
-
-        # the result is a JSON string:
-        print(y) 
+    def central_server_update(self, tray):
+        
+        
+        
+        
+        return
 
 class Tray:
     
-    def __init__(self, id, color, radius, time, havest):
+    def __init__(self, coodinates, cell, color, radius):
         self.id = id
+        self.coordinates = coodinates
         self.color = color
         self.radius = radius
-        self.time = time
-        self.havest = 0
+        self.time = date.today()
+        self.havest_score = 0
+
+    def update_sensors(self):
+        #here some magic happens
+        #color, radius and havest_score are updated
+        #updated_time = date.today() - time
+        #self.havest_score = self.Havest(updated_color, updated_radius, updated_time)
+        return
 
     def Havest(self, color, radius, time):
-        #color menor peso
-        #
-        return 
+        w_c = 0.2 
+        w_r = 0.3
+        w_t = 0.5
+        return w_c*pow(color, -1) + w_r*radius + w_t*time
 
