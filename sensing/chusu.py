1ff265ab4f0f05ce80058473b51b3bea39b6fea6
JuliaOli <maju.olivi@gmail.com>
2020-07-23 00:11:56 -0300

    update tray status function and csv file implemented

--- sensing/chusu.py
+++ sensing/chusu.py
@@ -1,48 +1,83 @@
 import json
 import numpy as np
+import pandas as pd 
 from datetime import date
-
+from time import sleep
 
 class Chusu:
 
     def __init__(self, chusu_size):
         self.chusu = np.zeros((3,3))
         self.trays_list = []
-
+        self.luminous = "780lx"
+        self.temperature = "28°C"
+        self.humidity = "70%"
+    
     def new_Tray(self):
-        # searching for an empty space for a new tray
-        empty_tray = np.where(self.chusu_trays == 0)
-        tray_index = list(zip(tray_index[0],tray_index[1]))
-        
-        # initiating a new Tray 
-        # after creating needs to update
-        tray = Tray(self.chusu.size + 1, tray_index[0], 0,0)
-        self.trays_list.append(tray)
+        if len(self.trays_list) < self.chusu.size:
+                
+            # searching for an empty space for a new tray
+            empty_tray = np.where(self.chusu_trays == 0)
+            tray_index = list(zip(tray_index[0],tray_index[1]))
+            
+            # initiating a new Tray 
+            # after creating needs to update
+            tray = Tray(self.chusu.size - len(self.trays_list), tray_index[0], 0,0)
+            self.trays_list.append(tray)
 
-        #updating cell status
-        self.chusu_trays[tray_index[0][0]][tray_index[0][1]] = 1
+            #updating cell status
+            self.chusu_trays[tray_index[0][0]][tray_index[0][1]] = 1
+        else:
+            print("Chusu it's already full, choose another one")
 
     def update_status(self):
+        #self.update_tray_status()
+        print(self.temperature + self.humidity + self.luminous)
+
+    def update_tray_status(self):
         for tray in self.trays_list:
             tray.update_sensors()
+        print(tray.id + tray.color + tray.radius + tray.havest_score + tray.time)
 
     def remove_tray(self, tray_id):
         #remove tray by id
         #status in the matrix will be updated to 0
         return
 
-    def central_server_update(self, tray):
-        
-        
-        
-        
-        return
+    def central_server_update(self):
+    
+    #    chusu_data = {
+    #        'id': tray.id, 
+    #        'coordinates': tray.coordinates,
+    #        'color': tray.color,
+    #        'radius': tray.radius,
+    #        'time': tray.time,
+    #        'havest_score': tray.havest_score
+    #    }
+    
+        chusu_data_frame = pd.DataFrame(columns=['id', 
+                                                'coordinates',
+                                                'color',
+                                                'radius',
+                                                'time',
+                                                'havest_score'])
+
+        for tray in self.trays_list:
+            chusu_data_frame.loc[tray] = [tray.id + 
+                                        tray.coordinates +
+                                        tray.color +
+                                        tray.radius +
+                                        tray.time +
+                                        tray.havest_score]
+
+        chusu_data_frame.to_csv('chusu_sensors_update.csv', float_format='%.2f')
+
 
 class Tray:
     
-    def __init__(self, coodinates, cell, color, radius):
+    def __init__(self, id, coordinates, color, radius):
         self.id = id
-        self.coordinates = coodinates
+        self.coordinates = coordinates
         self.color = color
         self.radius = radius
         self.time = date.today()
@@ -53,11 +88,28 @@ class Tray:
         #color, radius and havest_score are updated
         #updated_time = date.today() - time
         #self.havest_score = self.Havest(updated_color, updated_radius, updated_time)
-        return
+        updated_time = date.today() - time
+        self.havest_score = self.Havest(np.random.randint(255), np.random().rand() + np.random.randint(5), updated_time)
 
     def Havest(self, color, radius, time):
         w_c = 0.2 
         w_r = 0.3
         w_t = 0.5
         return w_c*pow(color, -1) + w_r*radius + w_t*time
+
+
+
+if __name__ == "__main__":
+
+    chusinho = Chusu(3)
+    chusinho.new_Tray()
+    chusinho.update_tray_status()
+
+    while(True):
+        chusinho.update_status()
+        sleep(10)
+        chusinho.update_tray_status()
+        sleep(10)
+        chusinho.central_server_update()
+        sleep(30)
 
